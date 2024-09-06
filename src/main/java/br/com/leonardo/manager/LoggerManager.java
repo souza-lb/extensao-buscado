b5a622deaaef63be943d58df4a3a70ae24a194f5
souza-lb <162514139+souza-lb@users.noreply.github.com>
2024-09-06 11:04:43 -0300

    Create LoggerManager.java
    Adicionada Classe para manipulação do Log da aplicação.

---
+++ src/main/java/br/com/leonardo/manager/LoggerManager.java
@@ -0,0 +1,35 @@
+package br.com.leonardo.manager;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerManager {
+    private static final String LOG_DIR = "log";
+    private static final Logger logger = Logger.getLogger(LoggerManager.class.getName());
+
+    public static void setupLogging() {
+        File logDir = new File(LOG_DIR);
+        if (!logDir.exists()) {
+            logDir.mkdir();
+        }
+
+        try {
+            FileHandler fileHandler = new FileHandler("./log/app.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logError(String message) {
+        logger.severe(message);
+    }
+}
